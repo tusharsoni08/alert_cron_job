84afde29356f18ba7542d829a98063d443047794
tusharsoni08 <soni.tushar93@gmail.com>
2018-08-17 23:44:06 +0530

    test commit

--- app.js
+++ app.js
@@ -38,8 +38,10 @@ app.get('/', function (req, res) {
 	            snapshot.forEach(doc => {
 	                let user_data = doc.data();
 	                console.log('Fetching train info...');
+	                let url_test = process.env['ENQUIRY_URL'] + user_data['train_number'] + '&startDate=' + user_data['boarding_date'] + '&journeyStn=' + user_data['station_code'] + '&journeyDate=' + user_data['arrival_date'] + '&boardDeboard=0&langFile=props.en-us'
+	                console.log(url_test)
 	                let options = {
-	                    uri: process.env['ENQUIRY_URL'] + user_data['train_number'] + '&startDate=' + user_data['boarding_date'] + '&journeyStn=' + user_data['station_code'] + '&journeyDate=' + user_data['arrival_date'] + '&boardDeboard=0&langFile=props.en-us',
+	                    uri: url_test,
 	                    transform: function (body) {
 	                        return cheerio.load(body);
 	                    }
